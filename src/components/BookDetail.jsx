9ef6a8e85c9cf1bce1f2a697862b1cb6e1b0e1e0
eileenhsiao <s111219026@stu.ntue.edu.tw>
2025-03-25 18:52:12 +0800

    update

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -17,11 +17,13 @@ function BookDetail({ book }) {
         <h2 className="text-white opacity-40 mb-1 text-lg">{book.author}</h2>
         <h1 className="text-white text-2xl font-bold mb-2">{book.title}</h1>
         <p className="text-white opacity-60 text-base mb-4">{book.summary}</p>
-      </div>
+      
       {/* 價格與按鈕 */}
       <div className="flex flex-col gap-4">
         <p className="text-2xl font-semibold">US${book.price}.00</p>
+        <p className="text-l font-semibold">stock：{book.stock}</p>
         <AddToCart />
+      </div>
       </div>
     </div>
   );
